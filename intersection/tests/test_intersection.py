4e84088f48f4f4171b3b78e786dc1678cdf27eb1
Emily Graber <egragraber@allegheny.edu>
2023-10-26 15:05:47 -0400

    update test assert

--- intersection/tests/test_intersection.py
+++ intersection/tests/test_intersection.py
@@ -14,7 +14,7 @@ def test_fill_random_container_list():
     """Ensure that generation of random container is correct size."""
     random_container = main.fill_random_container(10, 100, False)
     assert len(random_container) == 10
-    assert type(random_container) == list
+    assert isinstance(random_container, list)
 
 
 def test_intersection_list_single_double():
